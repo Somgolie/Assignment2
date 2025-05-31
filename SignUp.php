ad9f4f5799e251b102f5886557d57c757ea09da6
Somgolie <sejinkeonye@algomau.ca>
2025-05-31 04:50:38 +0000

    Added create account page

--- SignUp.php
+++ SignUp.php
@@ -1,24 +1,17 @@
-<?php 
+<?php  
+require_once('user.php');
 session_start();
-if (isset($_SESSION['authenticated'])){
-  header("Location: /index.php");
-}
-if(!isset($_SESSION['attempts'])){
-echo "";
-}
-else{
-  echo "Incorrect username or password. You have tried " . $_SESSION['attempts'] . " times.";
-}
-
+session_destroy();
+$user = new User();
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Log in</title>
+<title>SignUp</title>
 </head>
 <body>
 
-<h1>Log in to your account</h1>
+<h1>Create an Accout</h1>
 <form action="/validate.php" method="post">
   <label for="username">Username:</label><br>
   <input type="text" id="username" name="username" ><br>
@@ -26,7 +19,5 @@ else{
   <input type="password" id="password" name="password" ><br><br>
   <input type="submit" value="Submit">
 </form> 
-  <p><a href='./SignUp.php'>Create Account Here!</a></p>
-
 </body>
 </html> 
